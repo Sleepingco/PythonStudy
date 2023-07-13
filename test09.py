c80800bb106b88702ccf8545f811ee7e7fa593d8
sleepingco <hosuhosuhosu14@gmail.com>
2023-07-13 17:36:06 +0900

    20230713 #1

--- test09.py
+++ test09.py
@@ -2,6 +2,9 @@ import glob
 import os
 import random
 import statistics as sts
+from urllib.request import urlopen
+from datetime import date
+from datetime import datetime
 
 print(os.getcwd())  # get current working directory
 os.chdir('c:/PythonStudy/')  # change working directory
@@ -13,5 +16,10 @@ print(glob.glob('*.py'))
 print(random.choice(['John', 'Jane', 'James', 'Johanson', 'Joey']))
 print(random.sample(range(1, 45), 6), '+', random.sample(range(1, 9), 1))
 print(sts.mean([1, 2, 3, 4, 5]))
-
+ 재귀함수
 f = urlopen('https://www.naver.com')
+
+now = date.today()
+now1 = datetime.today()
+print(now)
+print(now1.strftime('%Y%m%d %H%M%S'))

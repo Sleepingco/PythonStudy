ff68717a5c24660e944397d65606c6a0646cd746
sleepingco <hosuhosuhosu14@gmail.com>
2023-07-14 17:37:09 +0900

    20230714 #1

--- test12-01.py
+++ test12-01.py
@@ -4,33 +4,33 @@ menu = test12.Menu()
 # 작업선택(M:메뉴관리, O:주문관리, S:매출관리, ''/X:종료
 while True:
   command = input('M:메뉴관리,O:주문관리,S:매출관리, ''/X:종료')
-  if command == 'x' or '':
+  if command.lower() == 'x'or '':
     break
-  elif command == 'm':
+  elif command .lower()== 'm':
     while True:
       order = input('추가:C 목록보기:R 수정:U 삭제:D 종료:''/X')
-      if order == 'x' or '':
+      if order =='' or order.lower() == 'x':
         break
-      elif order == 'c':
+      elif order.lower() == 'c' :
         menu.add()
-      elif order == 'r':
+      elif order.lower() == 'r' :
         menu.display()
-      elif order == 'u':
+      elif order.lower() == 'u':
         menu.update()
-      elif order == 'd':
+      elif order.lower() == 'd':
         menu.delete()
       else:
         print('잘못된 명령어 입니다')
-  elif command == 'o':
+  elif command.lower() == 'o':
     while True:
       order = input("주문하기:O 주문내역보기:R 주문관리종료:''")
-      if order == 'o':
+      if 'o' :
         pass
-      elif order == 'r':
+      elif order.lower() == 'r':
         pass
-      elif order == 'x' or '':
+      elif order.lower() == 'x' or '':
         break
-  elif command == 's':
+  elif command.lower() == 's':
     # 매출보기
     pass
   else:
